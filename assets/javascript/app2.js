a7481b5b30c83daf22fadf62330b1567bf65b238
sandtr0ut <47204349+sandtr0ut@users.noreply.github.com>
2019-04-03 21:12:04 -0700

    google api

--- assets/javascript/app2.js
+++ assets/javascript/app2.js
@@ -3,11 +3,11 @@
 function buildQueryURL() {
   
   //submit the term in the text-input to YouTube API
-  var query = $("#prodSearch")
+  var q = $("#prodSearch")
   .val()
   .trim();
   // ...but stringify it first
-  var q = JSON.stringify(query);
+  // var q = JSON.stringify(query);
   
   // this is a required parameter and contains what we need
   var part = "snippet";
@@ -20,7 +20,7 @@ function buildQueryURL() {
   
   // All combined for ajax request
   var queryURL = "https://www.googleapis.com/youtube/v3/search?" + "part=" + part + "&q=" + q + "&type=" + type + "&key=" + key;
-  
+  console.log(queryURL);
 
 }
 
@@ -34,14 +34,15 @@ $("#submit").on("click", function() {
 // build query url inside click function for ajax
  var queryURL = buildQueryURL();
  
- console.log(queryURL);
+ 
 
 // perform ajax request
-$.$.ajax({
+$.ajax({
   url: queryURL,
     method: "GET"
 })
 .then(function(response) {
-  var results = //start here
+  var results = response;
+  console.log(results);
 })
 });
